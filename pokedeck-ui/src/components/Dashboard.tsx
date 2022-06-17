b5246c9e40a6493e47fd5b9163b71062f97c5499
isaac harris <isaachrr4@gmail.com>
2022-06-17 15:19:41 -0400

    simple dashboard

--- pokedeck-ui/src/components/Dashboard.tsx
+++ pokedeck-ui/src/components/Dashboard.tsx
@@ -1,8 +1,19 @@
 import { User } from "../models/user";
+import { Navigate } from "react-router-dom";
 
-interface IDashboard {}
+interface IDashboard {
+    currentUser: User | undefined
+}
 
-function Dashboard (props: IDashboard) {}
+function Dashboard (props: IDashboard) {
+    return (
+        !props.currentUser ? <Navigate to="/login"/> :
+        <>
+            <h1>Welcome, {props.currentUser.username}!</h1>
+        </>
+    );
+
+}
 
 
 export default Dashboard;
