532a072fcd129e0761df7ecd19edca2943ef8eb1
isaac harris <isaachrr4@gmail.com>
2022-06-20 14:40:17 -0400

    DASHBOARD ALMOST COMPLETE

--- pokedeck-ui/src/components/Dashboard.tsx
+++ pokedeck-ui/src/components/Dashboard.tsx
@@ -1,16 +1,46 @@
 import { User } from "../models/user";
 import { Navigate } from "react-router-dom";
+import { Grid } from '@mui/material';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
+
 
 interface IDashboard {
     currentUser: User | undefined
 }
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+  
 
 function Dashboard (props: IDashboard) {
     return (
         !props.currentUser ? <Navigate to="/login"/> :
         <>
             <h1>Welcome, {props.currentUser.username}!</h1>
+            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+  <Grid item xs={6}>
+    <Item>1</Item>
+  </Grid>
+  <Grid item xs={6}>
+    <Item>2</Item>
+  </Grid>
+  <Grid item xs={6}>
+    <Item>3</Item>
+  </Grid>
+  <Grid item xs={6}>
+    <Item>4</Item>
+  </Grid>
+</Grid> 
+           
         </>
+        
     );
 
 }
