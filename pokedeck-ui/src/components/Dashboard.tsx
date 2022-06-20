dea623a391cbd428b9b5e069b7481b30f2c93260
isaac harris <isaachrr4@gmail.com>
2022-06-20 15:29:17 -0400

    dasboard

--- pokedeck-ui/src/components/Dashboard.tsx
+++ pokedeck-ui/src/components/Dashboard.tsx
@@ -25,19 +25,19 @@ function Dashboard (props: IDashboard) {
         <>
             <h1>Welcome, {props.currentUser.username}!</h1>
             <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-  <Grid item xs={6}>
-    <Item>1</Item>
-  </Grid>
-  <Grid item xs={6}>
-    <Item>2</Item>
-  </Grid>
-  <Grid item xs={6}>
-    <Item>3</Item>
-  </Grid>
-  <Grid item xs={6}>
-    <Item>4</Item>
-  </Grid>
-</Grid> 
+                <Grid item xs={6}>
+                    <Item>1</Item>
+                        </Grid>
+                        <Grid item xs={6}>
+                        <Item>2</Item>
+                            </Grid>
+                    <Grid item xs={6}>
+                        <Item>3</Item>
+                                </Grid>
+                         <Grid item xs={6}>
+                                    <Item>4</Item>
+                                            </Grid>
+                                </Grid> 
            
         </>
         
