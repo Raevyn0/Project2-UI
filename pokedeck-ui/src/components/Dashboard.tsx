b2bc6e0339a002f1a4ae072eeae7dda22be29073
ZacharyCooremans <77285580+ZacharyCooremans@users.noreply.github.com>
2022-07-05 16:25:11 -0500

    able to drect to search from dashboard

--- pokedeck-ui/src/components/Dashboard.tsx
+++ pokedeck-ui/src/components/Dashboard.tsx
@@ -3,6 +3,7 @@ import { Grid } from '@mui/material';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import { styled } from '@mui/material/styles';
+import { useNavigate } from "react-router-dom";
 
 
 interface IDashboard {
@@ -19,13 +20,18 @@ const Item = styled(Paper)(({ theme }) => ({
   
 
 function Dashboard (props: IDashboard) {
+
+    let navigate = useNavigate();
+
     return (
       
         <>
             <h1>Welcome to your PokeDeck Trainer!</h1>
             <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
                 <Grid item xs={6}>
-                    <Item>1</Item>
+                    <Item>
+                        <button onClick={() => navigate('/search')}>Look for a pokemon</button>
+                    </Item>
                         </Grid>
                         <Grid item xs={6}>
                         <Item>2</Item>
